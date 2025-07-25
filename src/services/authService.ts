98fcb6e4873df4ee973519779eaa3370a4bf76f5
melissalau17 <melissalau17300@gmail.com>
2025-07-25 18:52:52 +0700

    added new feature to Marketplace

--- src/services/authService.ts
+++
@@ -1,8 +0,0 @@
-import { api } from "@/lib/axios";
-
-export const authService = {
-  login: (data) => api.post("/auth/login", data),
-  register: (data) => api.post("/auth/register", data),
-  me: () => api.get("/auth/me"),
-  logout: () => api.post("/auth/logout"),
-};
